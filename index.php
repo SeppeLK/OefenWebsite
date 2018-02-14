f94c7f53f3a6cb4dc0bc879e04e5262c177500de
SeppeLK <seppe.gamecraft@gmail.com>
2018-02-14 17:05:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<title>Homepage van Seppe Van Schelstraete.</title>
+	<meta name="description" content="leren van php" />
+	<meta name="keywords" content="php, programmeren, dbz, 4e jaar" />
+	<meta name="author" content="Seppe Van Schelstraete" />
+	<meta name="Revisit-After" content="1 days" />
+	<meta http-equiv="Pragma" content="no-cache" />
+
+	<meta http-equiv="Cache-Control" content="no-cache, must-revalidate" />
+	<meta http-equiv="Expires" content="-1" />
+	<link rel="stylesheet" type="text/css" href="stylesheetdbz.css">
+</head>
+
+
+<body>
+
+      
+        
+    
+        
+   
+    
+   
+
+</body>
+</html>
