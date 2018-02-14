d3baeb22cc5b36c3b5744ee02e961466cae1a2ca
Driesboy <coenendries3@gmail.com>
2018-02-14 17:07:26 +0100

    reformatting

--- index.php
+++ index.php
@@ -1,30 +1,22 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
-	<title>Oefenpagina Seppe en Dries</title>
-	<meta name="description" content="leren van php" />
-	<meta name="keywords" content="php, programmeren, dbz, 4e jaar, gamecraftpe, Seppe Van Schelstraete, Dries Coenen" />
-	<meta name="author" content="Seppe Van Schelstraete, Dries Coenen" />
-	<meta name="Revisit-After" content="1 days" />
-	<meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1"/>
+    <title>Oefenpagina Seppe en Dries</title>
+    <meta name="description" content="leren van php"/>
+    <meta name="keywords" content="php, programmeren, dbz, 4e jaar, gamecraftpe, Seppe Van Schelstraete, Dries Coenen"/>
+    <meta name="author" content="Seppe Van Schelstraete, Dries Coenen"/>
+    <meta name="Revisit-After" content="1 days"/>
+    <meta http-equiv="Pragma" content="no-cache"/>
 
-	<meta http-equiv="Cache-Control" content="no-cache, must-revalidate" />
-	<meta http-equiv="Expires" content="-1" />
-	<link rel="stylesheet" type="text/css" href="stylesheetdbz.css">
+    <meta http-equiv="Cache-Control" content="no-cache, must-revalidate"/>
+    <meta http-equiv="Expires" content="-1"/>
+    <link rel="stylesheet" type="text/css" href="stylesheetdbz.css">
 </head>
 
 
 <body>
 
-      
-        
-    
-        
-   
-    
-   
 
 </body>
 </html>
