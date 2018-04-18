45d781347acd056c66ea77cd3b0d34c8c72da643
Driesboy <coenendries3@gmail.com>
2018-04-18 18:00:50 +0200

    Bootstrap4 and Jquery 3.1

--- preloader.js
+++ preloader.js
@@ -1,3 +1,13 @@
-$(window).load(function () {
-    $(".preloading").fadeOut("slow");
+$(window).on("load", function () {
+    $(".preloading").remove();
+});
+
+var headerHeight = 200;
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() < headerHeight) {
+        $('#myNav').removeClass('navbar-top').addClass('navbar-fixed-top');
+    } else {
+        $('#myNav').removeClass('navbar-fixed-top').addClass('navbar-top');
+    }
 });
