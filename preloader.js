eb1b697ce004cba5fb3bc2caab00b3d4a67306c3
Driesboy <coenendries3@gmail.com>
2018-04-30 11:59:33 +0200

    Revert "Bootstrap4 and Jquery 3.1"
    This reverts commit 45d781347acd056c66ea77cd3b0d34c8c72da643.

--- preloader.js
+++ preloader.js
@@ -1,13 +1,3 @@
-$(window).on("load", function () {
-    $(".preloading").remove();
-});
-
-var headerHeight = 200;
-
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() < headerHeight) {
-        $('#myNav').removeClass('navbar-top').addClass('navbar-fixed-top');
-    } else {
-        $('#myNav').removeClass('navbar-fixed-top').addClass('navbar-top');
-    }
+$(window).load(function () {
+    $(".preloading").fadeOut("slow");
 });
